75ece0616374d11828a4e81e3a20509abfd8c188
christophe-g <christophe.geiser@gmail.com>
2026-03-18 21:08:25 +0100

    refactor: reorganize YouTube component import and icon URL function for better structure

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,8 +4,21 @@ import './lite-youtube.css'
 import './styles.css'
 // import custom element used in markdown files
 
-// lapp-youtube
-import '@lit-app/cmp/youtube/youtube'
+import { Icon } from '@lit-app/cmp/icon/internal/icon'
+Icon.getURL = function (name, noFill) {
+  return `/images/material-symbols/${name}/materialsymbolsoutlined/${name}${noFill ? '' : '_fill1'}_${Icon.opticalSize}px.svg`
+}
 
+// lapp-youtube
+// import 'lite-youtube-embed'
+importYoutube()
+// import '@lit-app/cmp/youtube/youtube'
+// lapp-icon
+import '@lit-app/cmp/icon/icon'
 
 export default Theme
+
+function importYoutube() {
+  // We need to import the youtube component to make it available in markdown files
+  import('@lit-app/cmp/youtube/youtube')
+}
