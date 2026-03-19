ee684fbd76472657f0c52acdd0b0f8f33e1cd33f
christophe-g <christophe.geiser@gmail.com>
2026-03-19 09:46:16 +0100

    fix: update links in survey configuration for consistency and clarity

--- docs/app/survey/config.ts
+++ docs/app/survey/config.ts
@@ -34,7 +34,7 @@ function getSidebar() {
         { text: 'Create a new survey', link: `session-1-creating-a-new-survey.md` },
         { text: 'Add accessibility options', link: `session-2-adding-accessibility-options.md` },
         { text: 'Translate a survey', link: `session-3-localization.md` },
-        { text: 'Deploy a survey', link: `session-4-distribution.md` },
+        { text: 'Deploy a survey', link: `session-4-distributing.md` },
         { text: 'Analyze survey responses', link: `session-5-analyzing-survey-responses.md` },
 
       ]
@@ -50,56 +50,57 @@ function getSidebar() {
           items: [
             { text: 'Create a new survey', link: `creating-a-new-survey.md` },
             { text: 'Edit a survey', link: `editing-a-survey.md` },
-            { text: 'Test a survey', link: `testing-a-survey.md` },
+            { text: 'Test a form', link: `testing-a-form.md` },
             { text: 'Publish a survey', link: `publishing-a-survey.md` },
             { text: 'Mark a survey as favorite', link: `marking-a-survey-as-favorite.md` },
             { text: 'Delete a survey', link: `deleting-a-survey.md` },
             { text: 'Add content to a form', link: `adding-content-to-a-form.md` },
             { text: 'Provide rich formatting', link: `providing-rich-formatting.md` },
             { text: 'Use form logic', link: `logic-expression.md` },
-            { text: 'Set access rights for forms', link: `access-rights.md` },
+            { text: 'Add images to the image library', link: `adding-images-to-library.md` },
             { text: 'Share options across multiple questions', link: `sharing-options.md` },
             { text: 'Share logical expressions', link: `sharing-logical-expressions.md` },
             { text: 'Share content across multiple forms and surveys', link: `sharing-content.md` },
+            { text: 'Set access rights for forms', link: `access-rights.md` },
           ]
         },
         {
           text: 'Accessibility',
           collapsed: true,
           items: [
-            { text: 'Add an accessibility menu', link: `add-accessibility-menu.md` },
-            { text: 'Activate accessibility modes', link: `activate-accessibility-modes.md` },
-            { text: 'Use Easy Read', link: `use-easy-read.md` },
-            { text: 'Use Sign Language', link: `use-sign-language.md` },
-            { text: 'Use tooltips', link: `use-tooltips.md` },
+            { text: 'Add an accessibility menu', link: `adding-an-accessibility-menu.md` },
+            { text: 'Activate accessibility modes', link: `activating-accessibility-modes.md` },
+            { text: 'Use Easy Read', link: `easy-read.md` },
+            { text: 'Use Sign Language', link: `sign-language.md` },
+            { text: 'Use tooltips', link: `using-tooltips.md` },
           ]
         },
         {
           text: 'Localization',
           collapsed: true,
           items: [
-            { text: 'create a multilingual survey', link: `create-multilingual-survey.md` },
-            { text: 'Translate a survey', link: `translate-survey.md` },
-            { text: 'Use import/export to translate a survey', link: `use-import-export-to-translate-survey.md` },
+            { text: 'Create a multilingual survey', link: `creating-a-multilingual-survey.md` },
+            { text: 'Translate forms', link: `translate-forms.md` },
+            { text: 'Use import/export to translate forms', link: `using-import-export-to-translate-forms.md` },
           ]
         },
         {
           text: 'Distribution',
           collapsed: true,
           items: [
-            { text: 'Distribute a survey', link: `distribute-survey.md` },
-            { text: 'Create alias survey links', link: `create-alias-survey-links.md` },
-            { text: 'Use survey batches', link: `use-survey-batches.md` },
-            { text: 'Redirect respondents after completing a survey', link: `redirect-respondents-after-completing-survey.md` },
-            { text: 'Set survey terms', link: `set-survey-terms.md` },
+            { text: 'Distribute your survey', link: `generating-survey-links.md` },
+            { text: 'Create alias survey links', link: `creating-alias.md` },
+            { text: 'Use survey batches', link: `using-batches.md` },
+            { text: 'Redirect respondents after completing a survey', link: `redirection-after-completing-survey.md` },
+            { text: 'Set survey terms', link: `survey-terms.md` },
           ]
         },
         {
           text: 'Analytics and Export',
           collapsed: true,
           items: [
-            { text: 'Analyze survey responses', link: `analyze-survey-responses.md` },
-            { text: 'Export survey data', link: `export-survey-data.md` },
+            { text: 'Analyze survey data', link: `survey-analytics.md` },
+            { text: 'Export survey data', link: `exporting-survey-data.md` },
           ]
         },
       ]
@@ -121,29 +122,27 @@ function getSidebar() {
             { text: 'Localize', link: `localize.md` },
             { text: 'Image Library', link: `image-library.md` },
             { text: 'Prompts', link: `prompt.md` },
+            { text: 'Behavior', link: `behavior.md` },
             { text: 'Restore', link: `restore.md` },
-
           ]
         },
-        { text: 'Test Survey', link: `` },
+        { text: 'Test Survey', link: `test/index.md` },
         {
           text: 'Share Survey',
           collapsed: true,
           base: `${root}/reference/share/`,
           link: `index.md`,
           items: [
-
             { text: "Status", link: `status/index.md` },
             { text: "Publish", link: `publish/index.md` },
             { text: "Distribute", link: `distribute/index.md` },
-            { text: "Account Type", link: `account/index.md` },
+            { text: "Account Type", link: `account-type/index.md` },
             { text: "Redirection", link: `redirection/index.md` },
             { text: "Access", link: `access/index.md` },
-            { text: "Email", link: `message/index.md` },
+            { text: "Email", link: `email/index.md` },
             { text: "Batch", link: `batch/index.md` },
-            { text: "Webhooks", link: `webhooks/index.md` },
+            { text: "Webhooks", link: `webhook/index.md` },
             { text: "Terms", link: `terms/index.md` },
-            { text: 'Access Rights', link: `access/index.md` },
           ]
         },
         {
@@ -169,29 +168,27 @@ function getSidebar() {
         },
         {
           text: 'Architecture and Key Concepts', collapsed: true, items: [
-            { text: 'Understanding Survey, Form, Page, Section and Question Hierarchy', link: `` },
-            { text: 'How does the compose view work', link: `` },
-            { text: 'Building and Versioning', link: `` },
-            { text: 'Understanding Form Logic', link: `` },
-            { text: 'Test and Production Survey Links', link: `` },
-            { text: 'The Role of Batches', link: `` },
-            { text: 'Respondent accounts', link: `` },
-            { text: 'Using Survey Batches', link: `` },
+            { text: 'Understanding Survey, Form, Page, Section and Question Hierarchy', link: `understanding-survey-hierarchy.md` },
+            { text: 'How does the compose view work', link: `how-does-the-compose-view-work.md` },
+            { text: 'The concept of Modes', link: `the-concept-of-modes.md` },
+            { text: 'Building and Versioning', link: `survey-versionning.md` },
+            { text: 'Understanding Form Logic', link: `understanding-form-logic.md` },
+            { text: 'Test and Production Survey Links', link: `test-and-production-survey-links.md` },
+            { text: 'The Role of Batches', link: `the-role-of-batches.md` },
+            { text: 'Respondent accounts', link: `survey-respondent-accounts.md` },
+            { text: 'Using Survey Batches', link: `using-survey-batches.md` },
           ]
         }, {
           text: 'Survey Quality', collapsed: true, items: [
-            { text: 'How to choose the right question type', link: `` },
-            { text: 'Using Markdown', link: `` },
+            { text: 'How to choose the right question type', link: `choosing-the-right-question-type.md` },
+            { text: 'Using Markdown', link: `using-markdown.md` },
           ]
         }, {
           text: 'Data Privacy, security and Ethics', collapsed: true, items: [
-            { text: 'Data Privacy and Security', link: `` },
+            { text: 'Data Privacy and Security', link: `data-privacy-and-security.md` },
           ]
         }
-
-
       ]
-
     }
   ]
 }
